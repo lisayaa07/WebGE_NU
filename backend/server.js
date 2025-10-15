b2b3437dc57a730a347e30a172751081b800d1e0
lisayaa07 <chanipron07112546@gmail.com>
2025-10-15 18:49:11 +0700

    แก้

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,13 @@
-
-const cors = require("cors");
-const path = require("path");
-const express = require("express");
-
-const connection = require("./db");
-const db = connection.promise();  
-
-
-
-
-
-// server.js (ส่วนบน)
 require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const connection = require("./db");
+const db = connection.promise(); // ✅ ใช้ mysql2 แบบ promise
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 
 
