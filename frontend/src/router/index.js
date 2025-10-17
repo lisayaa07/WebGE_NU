55053276d71e3603b58690b02c17a8c09215db47
lisayaa07 <chanipron07112546@gmail.com>
2025-10-17 20:58:51 +0700

    /

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
 import MatchCaseView from '@/views/MatchCaseView.vue'
@@ -15,7 +15,7 @@ import test from '@/layout/test.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history:createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
